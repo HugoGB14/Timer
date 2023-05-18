236d42ad9938d6f5216621c5b042be5a451213d1
HUGO <129951007+HugoGB14@users.noreply.github.com>
2023-05-18 18:49:59 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(65, 70)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(59, 76)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "<"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(487, 70)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(55, 76)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = ">"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 42.0!)
+        Me.Label1.Location = New System.Drawing.Point(206, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(170, 79)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "0:00"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(157, 167)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 45)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Start"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(340, 167)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 45)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Reset"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 255)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Timer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private ReadOnly dataDirectory = AppDomain.CurrentDomain.BaseDirectory
+    Private Const Zero As Integer = 0
+    Dim Sec = 0
+    Dim Min = 0
+    Dim SSec
+    Dim SMin
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Sec -= 1
+        ReCon()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Sec += 1
+        ReCon()
+    End Sub
+
+    Public Sub ReCon()
+        'Conitions of the Variables'
+        If Sec = 60 Then
+            Min += 1
+            Sec = 0
+        ElseIf Sec < 0 Then
+            Min -= 1
+            Sec = 59
+        End If
+
+        If Min < 0 Then
+            Min = 0
+            Sec = 0
+        ElseIf Min > 60 Then
+            Min = 60
+        ElseIf Sec < 0 Then
+            Sec = 0
+        End If
+
+        'Conitions to the Refresh'
+
+        If Sec < 10 Then
+            Label1.Text = Min.ToString + ":" + "0" + Sec.ToString
+        Else
+            Label1.Text = Min.ToString + ":" + Sec.ToString
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SMin = Min
+        SSec = Sec
+        While Min > 0 Or Sec > 0
+            SSSS()
+            ReCon()
+            Slepp()
+        End While
+        My.Computer.Audio.Play(dataDirectory + "..\..\sounds\Zero.wav")
+    End Sub
+
+    Private Sub SSSS()
+        Sec -= 1
+    End Sub
+
+    Private Shared Sub Slepp()
+        Threading.Thread.Sleep(1000)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Min = SMin
+        Sec = SSec
+        ReCon()
+    End Sub
+End Class
